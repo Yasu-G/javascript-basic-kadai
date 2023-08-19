ec27bffb70a885cf5401c5e13755a63c0283645b
Yasu-G <140491519+Yasu-G@users.noreply.github.com>
2023-08-20 08:15:26 +0900

    Kadai005

---
+++ Kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 体重
+ let userWeight;
+ userWeight = 68
+
+ // 身長
+ let userHighest;
+ userHighest = 1.7
+
+
+//  Output
+console.log(userWeight)
+console.log(userHighest)
+console.log(userWeight / userHighest ** 2)
+console.log(userWeight / (userHighest * userHighest) )
